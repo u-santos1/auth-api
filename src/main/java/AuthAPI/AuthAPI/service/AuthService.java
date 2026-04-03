1447c75cd4656c7e0639fa34485c359f544f39b1
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-04-02 22:52:44 -0300

    git commit -m "feat(auth): implementa AutenticacaoService e integracao com UserDetailsService"

--- src/main/java/AuthAPI/AuthAPI/service/AuthService.java
+++ src/main/java/AuthAPI/AuthAPI/service/AuthService.java
@@ -1,5 +1,45 @@
 package AuthAPI.AuthAPI.service;
 
-public class AuthService {
+import AuthAPI.AuthAPI.dtos.requests.LoginDTO;
+import AuthAPI.AuthAPI.infra.segurity.TokenService;
+import AuthAPI.AuthAPI.model.Usuario;
+import AuthAPI.AuthAPI.repository.UsuarioRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.web.server.ServerHttpStrictTransportSecurityDsl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+public class AuthService implements UserDetailsService {
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    private final UsuarioRepository usuarioRepository;
+
+    public AuthService(AuthenticationManager authenticationManager, TokenService tokenService, UsuarioRepository usuarioRepository){
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o e-mail: " + username));
+    }
+
+    public String efetuarLogin(LoginDTO data){
+        var authenticationToken = new UsernamePasswordAuthenticationToken(data.email(), data.senha());
+        var authentication = authenticationManager.authenticate(authenticationToken);
+
+        Usuario usuarioAutenticado = (Usuario) authentication.getPrincipal();
+        if (!usuarioAutenticado.isEnabled()){
+            throw new DisabledException("Conta inativa");
+        }
+        return tokenService.gerarToken(usuarioAutenticado);
+    }
 }
